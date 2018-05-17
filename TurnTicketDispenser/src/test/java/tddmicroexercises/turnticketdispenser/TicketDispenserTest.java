531e04a7ce03a11c03035eb32036ff0e7aae8097
张晔10077668 <zhang.ye6@zte.com.cn>
2018-05-17 21:43:11 +0800

    init project TurnTicketDispense [master 01b

---
+++ TurnTicketDispenser/src/test/java/tddmicroexercises/turnticketdispenser/TicketDispenserTest.java
@@ -0,0 +1,13 @@
+package tddmicroexercises.turnticketdispenser;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TicketDispenserTest
+{
+    @Test
+    public void sample_test(){
+        assertEquals(1, 1);
+    }
+}
