d4de0743d97f558efdd271ef5dd04b9dbc9edc1f
张晔10077668 <zhang.ye6@zte.com.cn>
2018-05-19 15:51:43 +0800

    change the call of UnicodeFileToHtmlTextConverter

--- TextConverter/src/test/java/tddmicroexercises/textconvertor/UnicodeFileToHtmlTextConverterTest.java
+++ TextConverter/src/test/java/tddmicroexercises/textconvertor/UnicodeFileToHtmlTextConverterTest.java
@@ -22,14 +22,16 @@ public class UnicodeFileToHtmlTextConverterTest {
 
     @Test
     public void should_convert_greater_than_and_less_than() throws IOException{
-        UnicodeFileToHtmlTextConverter converter = new UnicodeFileToHtmlTextConverter(new StringReader(">_<|||"));
+        StringEscaper stringEscaper = new StringEscaper();
+        UnicodeFileToHtmlTextConverter converter = new UnicodeFileToHtmlTextConverter(new StringReader(">_<|||"), stringEscaper);
 
         assertEquals("&gt;_&lt;|||<br />", converter.convertToHtml());
     }
 
     @Test
     public void should_add_a_line_break_for_a_new_line() throws IOException {
-        UnicodeFileToHtmlTextConverter converter = new UnicodeFileToHtmlTextConverter(new StringReader("Cheers\nKael Zhang"));
+        StringEscaper stringEscaper = new StringEscaper();
+        UnicodeFileToHtmlTextConverter converter = new UnicodeFileToHtmlTextConverter(new StringReader("Cheers\nKael Zhang"), stringEscaper);
 
         assertEquals("Cheers<br />Kael Zhang<br />", converter.convertToHtml());
     }
