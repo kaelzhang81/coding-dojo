bfac9cb93e6579f33e4f1d8e9bb8f2ff11264813
kaelzhang81 <zhang.ye6@zte.com.cn>
2018-05-27 15:39:26 +0800

    refactor

--- orthogonalDesign/src/main/java/com/kaelzhang/kata/Product.java
+++ orthogonalDesign/src/main/java/com/kaelzhang/kata/Product.java
@@ -3,7 +3,13 @@ package com.kaelzhang.kata;
 import java.awt.Color;
 
 public class Product {
+    private final Color color;
+
+    public Product(Color color){
+        this.color = color;
+    }
+
     public Color getColor() {
-        return Color.RED;
+        return this.color;
     }
 }
--- orthogonalDesign/src/test/java/com/kaelzhang/kata/OrthogonalDesignTest.java
+++ orthogonalDesign/src/test/java/com/kaelzhang/kata/OrthogonalDesignTest.java
@@ -2,6 +2,7 @@ package com.kaelzhang.kata;
 
 import org.junit.Test;
 
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,14 +14,14 @@ public class OrthogonalDesignTest {
     @Test
     public void find_all_red_product(){
         List<Product> repo = new ArrayList();
-        repo.add(new Product());
+        repo.add(new Product(Color.RED));
         assertEquals(1, OrthogonalDesign.findAllRedProducts(repo).size());
     }
 
     @Test
     public void find_all_green_product() {
         List<Product> repo = new ArrayList();
-        repo.add(new Product());
+        repo.add(new Product(Color.GREEN));
         assertEquals(1, OrthogonalDesign.findAllGreenProducts(repo).size());
     }
 }
