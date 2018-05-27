203b9631b4fe0afb72aa187f53dcfcb4e4fc8694
kaelzhang81 <zhang.ye6@zte.com.cn>
2018-05-27 21:50:55 +0800

    add productRepo

--- orthogonalDesign/src/main/java/com/kaelzhang/kata/ProductRepository.java
+++ orthogonalDesign/src/main/java/com/kaelzhang/kata/ProductRepository.java
@@ -4,8 +4,14 @@ import java.awt.Color;
 import java.util.ArrayList;
 import java.util.List;
 
-public class OrthogonalDesign {
-    public static List<Product> findProducts(List<Product> repo, ProductSpec spec) {
+public class ProductRepository {
+    private List<Product> repo = new ArrayList<>();
+
+    public void add(Product product){
+        this.repo.add(product);
+    }
+
+    public List<Product> findProducts(ProductSpec spec) {
         List<Product> result = new ArrayList<>();
         for (Product product : repo) {
             if (spec.satisfy(product)) {
--- orthogonalDesign/src/test/java/com/kaelzhang/kata/ProductRepositoryTest.java
+++ orthogonalDesign/src/test/java/com/kaelzhang/kata/ProductRepositoryTest.java
@@ -1,5 +1,6 @@
 package com.kaelzhang.kata;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.awt.*;
@@ -10,28 +11,40 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 
-public class OrthogonalDesignTest {
+public class ProductRepositoryTest {
+    private ProductRepository repo;
+
+    @Before
+    public void initialize(){
+        repo = new ProductRepository();
+    }
+
     @Test
     public void find_all_red_products(){
         ProductSpec spec = new ColorSpec(Color.RED);
-        List<Product> repo = new ArrayList<>();
         repo.add(new Product(Color.RED, 20));
-        assertEquals(1, OrthogonalDesign.findProducts(repo, spec).size());
+        assertEquals(1, repo.findProducts(spec).size());
     }
 
     @Test
     public void find_all_green_products() {
         ProductSpec spec = new ColorSpec(Color.GREEN);
-        List<Product> repo = new ArrayList<>();
         repo.add(new Product(Color.GREEN, 5));
-        assertEquals(1, OrthogonalDesign.findProducts(repo, spec).size());
+        assertEquals(1, repo.findProducts(spec).size());
     }
 
     @Test
     public void find_all_weight_below_10_products() {
         ProductSpec spec = new WeightSpec(10);
-        List<Product> repo = new ArrayList<>();
         repo.add(new Product(Color.RED, 5));
-        assertEquals(1, OrthogonalDesign.findProducts(repo, spec).size());
+
+        assertEquals(1, repo.findProducts(spec).size());
+    }
+
+    @Test
+    public void find_products_weight_bewlow_10_and_color_is_green(){
+        ProductSpec spec = new WeightSpec(10);
+        repo.add(new Product(Color.RED, 5));
+        assertEquals(1, repo.findProducts(spec).size());
     }
 }
